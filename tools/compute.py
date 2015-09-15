8ea530723e2cbcd6245e444f4cb4a8b435c31787
Steven Irvin <steven dot irvin at gmail dot com>
2015-09-15 15:56:10 -0700

    m delete

--- tools/compute.py
+++ tools/compute.py
@@ -180,8 +180,8 @@ def notifier(_id, transport, messages, wait_after_msg, timeout):
         n1.info(ctxt, 'volume.delete', _volume_payload())
         n1.info(ctxt, 'compute.instance.create.start', _instance_payload())
         n1.info(ctxt, 'compute.instance.create.end', _instance_payload())
-        n1.info(ctxt, 'compute.instance.delete.start', _instance_payload())
-        n1.info(ctxt, 'compute.instance.delete.end', _instance_payload())
+        # n1.info(ctxt, 'compute.instance.delete.start', _instance_payload())
+        # n1.info(ctxt, 'compute.instance.delete.end', _instance_payload())
         if wait_after_msg > 0:
             time.sleep(wait_after_msg)
 
@@ -210,7 +210,6 @@ def _instance_payload():
             'state': 'active',
             'memory_mb': 8192,
             'disk_gb': 1024,
-            'metadata': ""
             }
 
 def _setup_logging(is_debug):
